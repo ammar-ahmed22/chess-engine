991d7b71c630ba8333616b69e9639f90acf83f26
Ammar Ahmed <ammar.ahmed1@uwaterloo.ca>
2024-03-06 16:59:33 -0500

    overload str2piece function to handle promotion value

--- src/utils/transform.ts
+++ src/utils/transform.ts
@@ -8,36 +8,69 @@ import Queen from "../Piece/Queen";
 import { ValueError } from "./error";
 import { SquareID } from "../SquareID";
 import { Piece } from "../Piece";
-import type { Color, MatrixType } from "../Chess";
+import type { Color, MatrixType, PieceType } from "../Chess";
 
+/**
+ * Converts a piece type to a piece object
+ * @param char The piece type
+ * @param pos The position of the piece
+ * @param color The color of the piece
+ */
+export function str2piece(char: PieceType, pos: SquareID, color: Color): Piece
 /**
  * Converts a FEN character to a piece
  * @param char The FEN character to convert (`Q, R, N, K, B, P` for white, `q, r, n, k, b, p` for black)
  * @param pos The position of the piece
  */
-export function str2piece(char: string, pos: SquareID): Piece {
-  if (char.length !== 1)
-    throw new ValueError("Only single characters allowed!", char);
-  const color: Color =
-    char.toLowerCase() !== char ? "white" : "black";
-  switch (char.toLowerCase()) {
-    case "p":
-      return new Pawn(color, pos);
-    case "r":
-      return new Rook(color, pos);
-    case "n":
-      return new Knight(color, pos);
-    case "b":
-      return new Bishop(color, pos);
-    case "k":
-      return new King(color, pos);
-    case "q":
-      return new Queen(color, pos);
-    default:
-      throw new ValueError(
-        `Cannot parse character: \`${char}\``,
-        char,
-      );
+export function str2piece(char: string, pos: SquareID): Piece 
+
+export function str2piece(...args: any[]): Piece {
+  if (args.length === 3) {
+    let piece: PieceType = args[0] as PieceType;
+    let pos: SquareID = args[1];
+    let color: Color = args[2] as Color;
+    switch (piece) {
+      case "pawn":
+        return new Pawn(color, pos);
+      case "rook":
+        return new Rook(color, pos);
+      case "knight":
+        return new Knight(color, pos);
+      case "king":
+        return new King(color, pos);
+      case "queen":
+        return new Queen(color, pos);
+      case "bishop":
+        return new Bishop(color, pos);
+      default:
+        throw new ValueError(`Cannot parse piece: \`${piece}\``, piece);
+    }
+  } else {
+    let char: string = args[0];
+    let pos: SquareID = args[1];
+    if (char.length !== 1)
+      throw new ValueError("Only single characters allowed!", char);
+    const color: Color =
+      char.toLowerCase() !== char ? "white" : "black";
+    switch (char.toLowerCase()) {
+      case "p":
+        return new Pawn(color, pos);
+      case "r":
+        return new Rook(color, pos);
+      case "n":
+        return new Knight(color, pos);
+      case "b":
+        return new Bishop(color, pos);
+      case "k":
+        return new King(color, pos);
+      case "q":
+        return new Queen(color, pos);
+      default:
+        throw new ValueError(
+          `Cannot parse character: \`${char}\``,
+          char,
+        );
+    }
   }
 }
 
